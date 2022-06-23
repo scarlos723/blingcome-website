0e67f68d51c1f52135021238061e5d9fdd678ec1
scarlos723 <carlossde58@gmail.com>
2022-06-23 13:59:23 -0500

    add routes and template

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import BlingcomeTemplate from '../containers/BlingcomeTemplate'
+import Home from '../pages/Home'
 import '../styles/App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div >
-      <h1>Welcome to Blingcome</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<BlingcomeTemplate></BlingcomeTemplate>}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
